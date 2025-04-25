2853bc7d18ae35a31226ac5346e8f1cad2fe381d
drod75 <dr507498@gmail.com>
2025-04-25 10:19:40 -0400

    Streamlit File basic setup

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+This file will use the datasets post cleaning, and display the results in a nice and neat format with user input 
+to create a streamlit application that can show different patterns help display high level analysis.
+'''
+
+import streamlit as st
+
+st.markdown("Hello World!")
